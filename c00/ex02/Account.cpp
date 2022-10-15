1f45aac83d145298808a3a79a409eb02a970b563
osyalcin <osmanyalcin989@gmail.com>
2022-10-15 13:26:20 +0300

    c00 is done

--- c00/ex02/Account.cpp
+++ c00/ex02/Account.cpp
@@ -55,7 +55,7 @@ int	Account::checkAmount(void) const
 
 void Account::_displayTimestamp()
 {
-	std::time_t t = std::time(0);   // get time now
+	std::time_t t = std::time(0);
     std::tm* now = std::localtime(&t);
     std::cout << "[" << (now->tm_year + 1900)
          << (now->tm_mon + 1)
