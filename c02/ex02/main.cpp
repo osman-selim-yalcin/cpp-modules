5bf018d22c07e0dc2c2310130e3704df4910abf8
osyalcin <osmanyalcin989@gmail.com>
2022-10-16 14:06:32 +0300

    ex02 done

--- c02/ex02/main.cpp
+++ c02/ex02/main.cpp
@@ -14,39 +14,4 @@ int main(void) {
 
 		std::cout << Fixed::max( a, b ) << std::endl;
 	}
-	// MY TESTS
-	{
-		std::cout << "\nMY TESTS:\n" << std::endl;
-	
-		Fixed a(4);
-		Fixed const b(2);
-	
-		std::cout << "a: " << a << std::endl;
-		std::cout << "b: " << b << std::endl;
-	
-		std::cout << "a++: " << a++ << std::endl;
-		std::cout << "a: " << a << std::endl;
-		std::cout << "++a: " << ++a << std::endl;
-		std::cout << "a: " << a << std::endl;
-	
-		std::cout << "a + b: " << a + b << std::endl;
-		std::cout << "b + a: " << b + a << std::endl;
-	
-		std::cout << "a - b: " << a - b << std::endl;
-		std::cout << "b - a: " << b - a << std::endl;
-	
-		std::cout << "a * b: " << a * b << std::endl;
-		std::cout << "b * a: " << b * a << std::endl;
-	
-		std::cout << "a / b: " << a / b << std::endl;
-		std::cout << "b / a: " << b / a << std::endl;
-	
-		std::cout << "max(a, b): " << Fixed::max(a, b) << std::endl;
-		std::cout << "max(b, a): " << Fixed::max(a, b) << std::endl;
-	
-		std::cout << "min(a, b): " << Fixed::min(a, b) << std::endl;
-		std::cout << "min(b, a): " << Fixed::min(a, b) << std::endl;
-	
-		return 0;
-	}
 }
